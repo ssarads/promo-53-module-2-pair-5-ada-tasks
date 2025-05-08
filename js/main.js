cd6cc103fc060baa8e00d454f8fd96309f26a431
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-05-08 10:49:11 +0200

    folder

---
+++ js/main.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const tasks = [
+  { name: "Recoger setas en el campo", completed: true, id: 1 },
+  { name: "Comprar pilas", completed: true, id: 2 },
+  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
+];
