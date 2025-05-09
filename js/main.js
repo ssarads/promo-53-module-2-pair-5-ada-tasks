cc53ffea13eb67283a1ed2ae5bf79fc9bb6323d1
Sara Delgado Saéz <sariiitaa.95@gmail.com>
2025-05-09 11:35:07 +0200

    cambios

--- js/main.js
+++ js/main.js
@@ -4,18 +4,32 @@ const tasks = [
   { name: "Recoger setas en el campo", completed: true, id: 1 },
   { name: "Comprar pilas", completed: true, id: 2 },
   { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-  {
-    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
-    id: 4,
-  },
+  { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id: 4 },
 ];
 
+// 1. Traer el ul del HTML -> JS querySelector
 const listTask = document.querySelector (".js_listtask");
+
+// 2. Ir rellenando con <li>
 for (const task of tasks) {
-listTask.innerHTML += `<li>${task.name}</li>`;
+  listTask.innerHTML += `<li class="js_task"> <input type="checkbox"  >${task.name}</li>`;
 }
 
+// 3. Traer todos los <li> y ponerles un evento
+
+const arrayDeElementosLi = document.querySelectorAll('.js_task');
+
+for( const li of arrayDeElementosLi ) {
+  li.addEventListener("click",(ev)=>{
+    //console.log(ev.target);
+    //console.log(ev.currentTarget);
+    //console.log(li)
+    //console.log(arrayDeElementosLi);
+    ev.currentTarget.classList.toggle("tachado")
+  });
+}
+
+// NO: arrayDeElementosLi.addEventListener -> El array no tiene addEventListener
 
 
 
