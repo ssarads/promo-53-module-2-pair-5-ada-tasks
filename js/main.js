86155a750f48e62fe809aa5dc5f1d6be8e09a75b
JessicaVR86 <jessica.vld.rdi@gmail.com>
2025-05-12 11:30:38 +0200

    condicional

--- js/main.js
+++ js/main.js
@@ -1,38 +1,62 @@
-'use strict';
+"use strict";
 
-const tasks = [
+let tasks = [
   { name: "Recoger setas en el campo", completed: true, id: 1 },
   { name: "Comprar pilas", completed: true, id: 2 },
-  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-  { name: "Aprender cómo se realizan las peticiones al servidor en JavaScript", completed: false, id: 4 },
+  { name: "Poner una lavadora de blancos", completed: false, id: 3 },
+  {
+    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
+    completed: false,
+    id: 4,
+  },
 ];
 
 // 1. Traer el ul del HTML -> JS querySelector
-const listTask = document.querySelector (".js_listtask");
+const listTask = document.querySelector(".js_listtask");
 
 // 2. Ir rellenando con <li>
 for (const task of tasks) {
-  listTask.innerHTML += `<li class="js_task"> <input type="checkbox"  >${task.name}</li>`;
+  console.log(task);
+  
+  if(task.completed === true) {
+    listTask.innerHTML += `<li class="js_task tachado"> <input type="checkbox" checked >${task.name}</li>`;
+  }
+  else {
+    listTask.innerHTML += `<li class="js_task"> <input type="checkbox"  >${task.name}</li>`;
+  }
+
 }
 
 // 3. Traer todos los <li> y ponerles un evento
 
-const arrayDeElementosLi = document.querySelectorAll('.js_task');
+const arrayDeElementosLi = document.querySelectorAll(".js_task");
 
-for( const li of arrayDeElementosLi ) {
-  li.addEventListener("click",(ev)=>{
+for (const li of arrayDeElementosLi) {
+  li.addEventListener("click", (ev) => {
     //console.log(ev.target);
     //console.log(ev.currentTarget);
     //console.log(li)
     //console.log(arrayDeElementosLi);
-    ev.currentTarget.classList.toggle("tachado")
+    ev.currentTarget.classList.toggle("tachado");
   });
 }
 
 // NO: arrayDeElementosLi.addEventListener -> El array no tiene addEventListener
 
-listTask.addEventListener('click' , () =>{
-  listTask.classList.toggle('tachado')
-});
+// listTask.addEventListener('click' , () =>{
+//   listTask.classList.toggle('tachado')
+// });
 
+const handleClickList = (event) => {
+  const taskId = parseInt(event.target.id); // Obtengo el id del checkbox clickado por la usuaria
+  if (!taskId) return; // Si no ha pulsado en el checkbox, no queremos hacer nada y salimos de la función
+
+  // Busca la tarea que tenga el id `taskId` en el array `tasks`
+  // Una vez que has obtenido la tarea, actualiza la propiedad `completed`
+  // Pinta de nuevo las tareas en el html
+};
+
+list.addEventListener("click", handleClickList);
+
+///  2.12  PETICIONES SERVIDOR - FETCH
 
